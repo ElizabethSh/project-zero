42e0ddb05699c3e6f846fe152f2908a0bc4a4fbb
Lisa <liza.shoshkina@gmail.com>
2025-06-25 16:15:45 +0200

    WIP: add styles for the Layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,16 +1,20 @@
 import Footer from "./footer";
 import Header from "./header";
 
+import "./layout.scss";
+
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 const Layout: React.FC<LayoutProps> = ({ children }) => (
-  <>
+  <div className="layout">
     <Header />
-    <main>{children}</main>
+    <main>
+      <div className="container">{children}</div>
+    </main>
     <Footer />
-  </>
+  </div>
 );
 
 export default Layout;
