53d1d4795a07e533a386df7ac21b20187247fc09
Lisa <liza.shoshkina@gmail.com>
2025-06-25 15:38:10 +0200

    WIP: build layout of the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import Layout from "@components/layout";
+import HomePage from "@pages/home";
+
 function App() {
-  return <div>New app</div>;
+  return (
+    <Layout>
+      <h1>Welcome to My App</h1>
+      <p>This is a simple React application.</p>
+      <HomePage />
+    </Layout>
+  );
 }
 
 export default App;
