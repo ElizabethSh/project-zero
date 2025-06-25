37200b2b339d90ff90d591df1625a04ed105409f
Lisa <liza.shoshkina@gmail.com>
2025-06-25 15:37:26 +0200

    Add HomePage

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+const HomePage = () => (
+  <section>
+    <h1>Hi, I'm Lisa Frontend developer</h1>
+    <img
+      src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      alt="Lisa Frontend developer"
+      width="600"
+    />
+  </section>
+);
+
+export default HomePage;
