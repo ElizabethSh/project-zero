2a601b4657ef926bc2631df3d38284dc6e91733c
Lisa <liza.shoshkina@gmail.com>
2025-06-25 15:37:07 +0200

    Add the Footer component

---
+++ src/components/layout/footer/index.tsx
@@ -0,0 +1,17 @@
+import { linkedin, mail } from "../../../icons";
+
+const Footer = () => (
+  <footer>
+    <p>Contact with me</p>
+    <ul>
+      <li>
+        <a>{linkedin}</a>
+      </li>
+      <li>
+        <a>{mail}</a>
+      </li>
+    </ul>
+  </footer>
+);
+
+export default Footer;
