d30951415cee5e0dce68d956973ae39a4a9980bf
Lisa <liza.shoshkina@gmail.com>
2025-06-26 11:51:28 +0200

    Add styles for the Home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,21 @@
-const HomePage = () => (
-  <section>
-    <h1>Hi, I'm Lisa Frontend developer</h1>
+import "./home.scss";
+
+const HomePage: React.FC = () => (
+  <section className="home">
     <img
+      className="home__image"
       src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       alt="Lisa Frontend developer"
-      width="600"
+      width="500"
     />
+    <div className="home__content">
+      <h1 className="home__title">
+        Hi, I'm Lisa <br /> Frontend developer
+      </h1>
+      <p>
+        I build engaging and accessible web experiences with React & TypeScript.
+      </p>
+    </div>
   </section>
 );
 
