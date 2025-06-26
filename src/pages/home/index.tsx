deab9d208aa3843285a8a80b765002536a5db609
Lisa <liza.shoshkina@gmail.com>
2025-06-26 16:26:03 +0200

    Add id to the Home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import "./home.scss";
 
 const HomePage: React.FC = () => (
-  <section className="home">
+  <section className="home" id="home">
     <img
       className="home__image"
       src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
