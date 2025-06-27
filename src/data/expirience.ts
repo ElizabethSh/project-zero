2c9d18f988084046fb08f55b882792e1816f0796
Lisa <liza.shoshkina@gmail.com>
2025-06-27 11:34:50 +0200

    Add data and types: work expirience

---
+++ src/data/expirience.ts
@@ -0,0 +1,43 @@
+import type { Expirience } from "types/index";
+
+export const expirience: Expirience[] = [
+  {
+    id: "1",
+    company: "Veo Technologies ApS",
+    link: "https://veo.co",
+    position: "Software Engineer",
+    startDate: "Jan 2022",
+    endDate: undefined,
+    description:
+      "Developed and maintained web applications using React, Typescript and JavaScript.",
+    skills: [
+      "TypeScript",
+      "React",
+      "JavaScript",
+      "Redux",
+      "HTML & SCSS",
+      "Storybook",
+      "Jest",
+      "Vitest",
+      "React Testing Library",
+    ],
+  },
+  {
+    id: "2",
+    company: "HTML Academy",
+    link: "https://htmlacademy.ru",
+    position: "Frontend Developer Student",
+    startDate: "Sept 2019",
+    endDate: "Apr 2021",
+    description:
+      "During my education, I solidified my skills in React, JavaScript, HTML, and CSS through the hands-on development of 5 distinct projects.",
+    skills: [
+      "React",
+      "JavaScript",
+      "HTML & SCSS",
+      "Jest",
+      "React Testing Library",
+      "Redux",
+    ],
+  },
+];
---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type Expirience = {
+  id: string;
+  link: string;
+  company: string;
+  position: string;
+  startDate: string;
+  endDate?: string;
+  description: string;
+  skills: string[];
+};
