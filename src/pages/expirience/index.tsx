908f7a7733ecf741071ef796d0c6a345f402cfbd
Lisa <liza.shoshkina@gmail.com>
2025-06-27 12:39:43 +0200

    Add styles for the Expirience page

--- src/pages/expirience/index.tsx
+++ src/pages/expirience/index.tsx
@@ -1,8 +1,11 @@
 import { expirience } from "data/expirience";
 
+import "./expirience.scss";
+
 const ExpiriencePage: React.FC = () => (
-  <section>
-    <ul>
+  <section className="experience" id="experience">
+    <h2 className="section-header">Experience</h2>
+    <ul className="experience__list">
       {expirience.map(
         ({
           id,
@@ -14,18 +17,27 @@ const ExpiriencePage: React.FC = () => (
           description,
           skills,
         }) => (
-          <li key={id}>
-            <a href={link} target="_blank" rel="noopener noreferrer">
-              <p>
+          <li key={id} className="experience__item">
+            <a
+              className="experience__company-link"
+              href={link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p className="experience__date">
                 {startDate} - {endDate || "present"}
               </p>
-              <h3>{`${position}, ${company}`}</h3>
-              <p>{description}</p>
-              <ul>
-                {skills.map((skill) => (
-                  <li key={skill}>{skill}</li>
-                ))}
-              </ul>
+              <div className="experience__content">
+                <h3 className="experience__title">{`${position}, ${company}`}</h3>
+                <p className="experience__description">{description}</p>
+                <ul className="experience__skills skills">
+                  {skills.map((skill) => (
+                    <li className="skill" key={skill}>
+                      {skill}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </a>
           </li>
         )
