158ce007365bd0ff7a4f6ae1ba6719b63ac28087
Lisa <liza.shoshkina@gmail.com>
2025-07-01 16:15:48 +0200

    Add styles for the active link

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,24 +1,33 @@
+import { useState } from "react";
+
 import "./header.scss";
 
 const PAGES = ["home", "about", "experience", "projects"] as const;
 
-const Header = () => (
-  <header className="header">
-    <div className="container">
-      {/* TODO: add logo? */}
-      <nav className="nav">
-        <ul className="nav-list">
-          {PAGES.map((page) => (
-            <li key={page}>
-              <a className="page-link" href={`#${page}`}>
-                {page}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </div>
-  </header>
-);
+const Header = () => {
+  const [activeLink, setActiveLink] = useState<string>(PAGES[0]);
+
+  return (
+    <header className="header">
+      <div className="container">
+        <nav className="nav">
+          <ul className="nav-list">
+            {PAGES.map((page) => (
+              <li key={page}>
+                <a
+                  className={`page-link ${activeLink === page ? "active" : ""}`}
+                  href={`#${page}`}
+                  onClick={() => setActiveLink(page)}
+                >
+                  {page}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
